c13dfa158ca91ad27a25b68ae76d87e92eb2e6b2
thyme <thymejoe@gmail.com>
2020-11-13 11:03:41 +0800

    first commit

--- src/main/java/edu/gd/mvndemo/LoginServlet.java
+++ src/main/java/edu/gd/mvndemo/LoginServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+
+// 登录 servlet
 @WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
